c86a53d710ec1731f1a14f018c109ec5bebd5979
Cooper Nichols <coopnich92@gmail.com>
2020-03-06 08:50:43 -0600

    navbar skeleton and login/logout added

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,25 @@
+import { Route, } from "react-router-dom";
+import React from "react";
+import Login from "./auth/Login";
+import Home from "./home/Home";
+
+const ApplicationViews = (props) => {
+    const hasUser = props.hasUser;
+    const setUser = props.setUser;
+    return (
+      <React.Fragment>
+        <Route path="/login" render={props => {
+          return <Login setUser={setUser} {...props} />
+        }} />
+        <Route
+          exact
+          path="/ "
+          render={props => {
+            return <Home />;
+          }}
+        />
+      </React.Fragment>
+    );
+  };
+  
+  export default ApplicationViews;
