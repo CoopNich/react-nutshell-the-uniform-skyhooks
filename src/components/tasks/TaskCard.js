47a1c69b8dc8c97099273c6287fbfd5a1b610a05
Chase Sullins <jcsullins222@yahoo.com>
2020-03-09 21:29:52 -0500

    when you edit a task, it will no longer lose its userId

--- src/components/tasks/TaskCard.js
+++ src/components/tasks/TaskCard.js
@@ -1,51 +1,47 @@
 import React from "react";
 
 const TaskCard = props => {
-  const handleCheckboxChange = () => {
-    console.log("clicked checkbox");
+  //function that changes isComplete from false to true onChange, and then re-renders the task list
+  const taskComplete = () => {
+    console.log("Clicked Checkbox");
   };
 
-      return (
-      <div className="card">
-        <div className="card-content">
-          <h3>
-            <span className="card-taskname">
-              <p
-                onClick={() =>
-                  props.history.push(`/tasks/${props.task.id}/edit`)
-                }
-              >
-                {props.task.name}
-              </p>
-            </span>
-          </h3>
-          <p>Completion Date: {props.task.completionDate}</p>
-          {props.deleteTask && (
-            <button
-              type="button"
-              onClick={() => {
-                if (
-                  window.confirm("Are you sure you wish to delete this item?")
-                )
-                  props.deleteTask(props.task.id);
-              }}
+  return (
+    <div className="card">
+      <div className="card-content">
+        <h3>
+          <span className="card-taskname">
+            <p
+              onClick={() => props.history.push(`/tasks/${props.task.id}/edit`)}
             >
-              Delete
-            </button>
-          )}
-          <div className="checkbox">
-            <label>
-              <input
-                type="checkbox"
-                value="isComplete"
-                // checked={}
-                onChange={handleCheckboxChange}
-              />
-              Complete
-            </label>
-          </div>
+              {props.task.name}
+            </p>
+          </span>
+        </h3>
+        <p>Completion Date: {props.task.completionDate}</p>
+        {props.deleteTask && (
+          <button
+            type="button"
+            onClick={() => {
+              if (window.confirm("Are you sure you wish to delete this item?"))
+                props.deleteTask(props.task.id);
+            }}
+          >
+            Delete
+          </button>
+        )}
+        <div className="checkbox">
+          <label>
+            <input
+              type="checkbox"
+              value={false}
+              onChange={taskComplete}
+            />
+            Complete
+          </label>
         </div>
       </div>
-    );
- };
+    </div>
+  );
+};
 export default TaskCard;
