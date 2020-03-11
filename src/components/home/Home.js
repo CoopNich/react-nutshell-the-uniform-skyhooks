a2256fd8ac4b6a7b64421b5c911e287cc2014b2f
Cooper Nichols <coopnich92@gmail.com>
2020-03-11 10:00:49 -0500

    Welcome user added

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,11 +1,27 @@
 import React, { useState, useEffect } from "react";
+import UserManager from "../../modules/UserManager"
+
+
 
 const Home = () => {
+
+      const [user, setUser] = useState({username: ""});
+
+    const getCurrentUser = () => {
+        return UserManager.getCurrentUser().then(user => {
+            setUser(user)
+        });
+    };
+
+
+    useEffect(() => {
+        getCurrentUser();
+    }, []);
 
   return (
     <>
   
-      <h1>Home</h1>
+      <h1>Welcome {user.username}</h1>
     </>
   );
 };
